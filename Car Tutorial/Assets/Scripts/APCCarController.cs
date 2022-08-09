7c4a04fc3dd384a138be88483c13b589c53f162d
Mitchell Burkhardt <MABurkhardt@student.fullsail.edu>
2022-08-09 11:50:36 -0400

    Asset Time

---
+++ Car Tutorial/Assets/Scripts/APCCarController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APCCarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float currentBreakForce;
+    private bool isBreaking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [SerializeField] private WheelCollider frontLeftCollider;
+    [SerializeField] private WheelCollider frontRightCollider;
+    [SerializeField] private WheelCollider rearLeftCollider;
+    [SerializeField] private WheelCollider rearRightCollider;
+
+    [SerializeField] private Transform frontLeftTransform;
+    [SerializeField] private Transform frontRightTransform;
+    [SerializeField] private Transform rearLeftTransform;
+    [SerializeField] private Transform rearRightTransform;
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        currentBreakForce = isBreaking ? breakForce : 0f;
+
+        frontLeftCollider.motorTorque = verticalInput * motorForce;
+        frontRightCollider.motorTorque = verticalInput * motorForce;
+        rearLeftCollider.motorTorque = verticalInput * motorForce;
+        rearRightCollider.motorTorque = verticalInput * motorForce;
+
+        ApplyBreaking();
+    }
+
+    private void ApplyBreaking()
+    {
+        frontLeftCollider.brakeTorque = currentBreakForce;
+        frontRightCollider.brakeTorque = currentBreakForce;
+        rearLeftCollider.brakeTorque = currentBreakForce;
+        rearRightCollider.brakeTorque = currentBreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftCollider.steerAngle = currentSteerAngle;
+        frontRightCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftCollider, frontLeftTransform);
+        UpdateSingleWheel(frontRightCollider, frontRightTransform);
+        UpdateSingleWheel(rearLeftCollider, rearLeftTransform);
+        UpdateSingleWheel(rearRightCollider, rearRightTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
---
+++ Car Tutorial/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; //info about axles
+    public float maxMotorTorque; //max torque the motor can apply to a wheel
+    public float maxSteeringAngle; //max steering angle the wheel can have
+
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+
+            if (axleInfo.motorized)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motorized; //attached to a motor?
+    public bool steering; //does this axel steer?
+}
